c8d025be90900145fe7a35d5a544f4cded9bac7a
Adam <adambh@outlook.com>
2019-01-18 00:45:52 +0200

    First code changes commit
    Overhauled base code

--- kair.go
+++ kair.go
@@ -1,31 +1,4 @@
-// Copyright 2018 Guilherme Caruso. All rights reserved.
-// License that can be found in the LICENSE file.
-
-/*
-Package Kair Used to facilitate the process of formatting dates and times using human language in your process.
-
-Examples:
-
-> If you need to get the current time:
-	Kair.Now().Time
-
-> If you need to get a formatted date:
-	Kair.Date().Time
-
-> If you need to get a formatted datetime:
-	Kair.DateTime().Time
-
-
-> Using standart formatters:
-	Kair.DateTime("20,05,2018,10,20,00").Format("LT") // "10:20 AM"
-	Kair.DateTime("20,05,2018,10,20,00").Format("LL") // "29/5/2018"
-	Kair.DateTime("20,05,2018,10,20,00").Format("llll") // "Mon, May 29, 2018 10:20 AM"
-
--> Using custom formatter:
-	Kair.DateTime("20,05,2018,10,20,00").PersonalFormat(""MMM/dd/YY h:m:s"") // "May/20/18 10:20:0"
-*/
-
-package kair
+package sharpdate
 
 import (
 	"regexp"
@@ -33,8 +6,33 @@ import (
 	"time"
 )
 
+// DateTime Represents an instant in time, typically expressed as a date and time of day.
+//
+// Arguments:
+// year: int
+// month: int
+// day: int
+// hour: int
+// min: int
+// sec: int
+// nsec: int
+// location: *time.Location
+//
+// Properties:
+// Time: time.Time
+type DateTime struct {
+	Year     int
+	Month    int
+	Day      int
+	Hour     int
+	Min      int
+	Sec      int
+	Nsec     int
+	Location *time.Location
+	Time     time.Time
+}
+
 var (
-	units  = []string{"LT", "LTS", "L", "l", "LL", "ll", "LLL", "lll", "LLLL", "llll"}
 	months = map[int]time.Month{
 		1:  time.January,
 		2:  time.February,
@@ -49,125 +47,210 @@ var (
 		11: time.November,
 		12: time.December,
 	}
-	formats = map[string]string{
+
+	predefs = map[string]string{
+		"dddd": "Monday",
+		"ddd":  "Mon",
+		"dd":   "02",
+		"d":    "2",
 		"MMMM": "January",
 		"MMM":  "Jan",
 		"MM":   "01",
 		"M":    "1",
-		"YYYY": "2006",
-		"YY":   "06",
-		"DD":   "Monday",
-		"D":    "Mon",
-		"dd":   "02",
-		"d":    "2",
+		"yyyy": "2006",
+		"yy":   "06",
+		"HH":   "15",
 		"hh":   "03",
 		"h":    "3",
 		"mm":   "04",
 		"m":    "4",
 		"ss":   "05",
 		"s":    "5",
+		"f":    "000",
+		"ff":   "999999",
+		"TT":   "PM",
+		"tt":   "pm",
+		"Z":    "MST",
+		"zz":   "-07",
+		"zzz":  "-07:00",
 	}
 )
 
-//SKair Used to standardize the use of functions
-type SKair struct {
-	Time time.Time
-}
+// ToString converts the value of the current DateTime instance to its equivalent string representation using the specified format.
+//
+// Avaialble formats:
+// dddd: day string. example: Monday
+// ddd:  short day string, example: Mon
+// dd:   day number, from 01 to (end of month). example: 02
+// d:    day number, from 1 to (end of month). example: 2
+// MMMM: month string. example: January
+// MMM:  short month string. example: Jan
+// MM:   month number, from 01 to 12. example: 01
+// M:    month number, from 1 to 12. example: 1
+// yyyy: year. example: 2006
+// yy:   year, last 2 digits. example: 06
+// HH:   hours, from 00 to 23. example: 15
+// hh:   hours, from 01 to 12. example: 03
+// h:    hours, from 0 to 12. example: 3
+// mm:   minutes. example: 04
+// m:    minutes. example: 4
+// ss:   seconds. example: 05
+// s:    seconds. example: 5
+// f:	 milliseconds. example: .000
+// ff:	 microseconds. example: .999999
+// tt:	 meridian designation. example: pm/am
+// TT:	 meridian designation. example: PM/AM
+// Z: 	 Timezone. example: MST
+// zz:	 Timezone offset. example: -07
+// zzz:	 Timezone offset. example: -07:00
 
-/*
-Format Uses standard sequence for the time format.
-Returns a string standart format if var is invalid
-
-Standard formats :
-	["LT", "LTS", "L", "l", "LL", "ll", "LLL", "lll", "LLLL", "llll"]
+/* Avaialble formats:
+dddd: day string. example: Monday
+ddd:  short day string, example: Mon
+dd:   day number, from 01 to (end of month). example: 02
+d:    day number, from 1 to (end of month). example: 2
+MMMM: month string. example: January
+MMM:  short month string. example: Jan
+MM:   month number, from 01 to 12. example: 01
+M:    month number, from 1 to 12. example: 1
+yyyy: year. example: 2006
+yy:   year, last 2 digits. example: 06
+HH:   hours, from 00 to 23. example: 15
+hh:   hours, from 01 to 12. example: 03
+h:    hours, from 0 to 12. example: 3
+mm:   minutes. example: 04
+m:    minutes. example: 4
+ss:   seconds. example: 05
+s:    seconds. example: 5
+f:	    milliseconds. example: .000
+ff:	 microseconds. example: .999999
+tt:	 meridian designation. example: pm/am
+TT:	 meridian designation. example: PM/AM
+Z: 	 Timezone. example: MST
+zz:	 Timezone offset. example: -07
+zzz:	 Timezone offset. example: -07:00
 */
-func (k *SKair) Format(format string) string {
-	timeLint := k.Time
-	var timeriz string
-	switch format {
-	case units[0]:
-		timeriz = timeLint.Format("3:04 PM")
-	case units[1]:
-		timeriz = timeLint.Format("3:04:05 PM")
-	case units[2]:
-		timeriz = timeLint.Format("02/01/2006")
-	case units[3]:
-		timeriz = timeLint.Format("02/1/2006")
-	case units[4]:
-		timeriz = timeLint.Format("January 2, 2006")
-	case units[5]:
-		timeriz = timeLint.Format("Jan 2, 2006")
-	case units[6]:
-		timeriz = timeLint.Format("January 2, 2006 3:04 PM")
-	case units[7]:
-		timeriz = timeLint.Format("Jan 2, 2006 3:04 PM")
-	case units[8]:
-		timeriz = timeLint.Format("Monday, January 2, 2006 3:04 PM")
-	case units[9]:
-		timeriz = timeLint.Format("Mon, Jan 2, 2006 3:04 PM")
-	default:
-		timeriz = timeLint.String()
-	}
-	return timeriz
-}
-
-/*
-CustomFormat Uses custom sequence for the time format.
-
-Returns a string custom datetime format
-
-Custom formatters :
-	"MMMM": Long Month,
-	"MMM":  Month,
-	"MM":   Zero Number Month,
-	"M":    Number Month,
-	"YYYY": Long Year,
-	"YY":   Year,
-	"DD":   Long Day,
-	"D":    Day,
-	"dd":  	Long Number Day,
-	"d":    Number Day,
-	"hh":   Long Hour,
-	"h":   	Hour,
-	"mm":   Long Minute,
-	"m":    Minute,
-	"ss":   Long Second,
-	"s":    Second
-
-*/
-func (k *SKair) CustomFormat(pformat string) string {
-	re := regexp.MustCompile(`(?m)(M{4})|(M{3})|(M{2})|(M{1})|(Y{4})|(Y{2})|(D{2})|(D{1})|(d{2})|(d{1})|(h{2})|(h{1})|(m{2})|(m{1})|(s{2})|(s{1})`)
-
-	for _, match := range re.FindAllString(pformat, -1) {
-		for key, val := range formats {
+func (d DateTime) ToString(resultFormat string) string {
+	regex := regexp.MustCompile(`(?m)(M{4})|(M{3})|(M{2})|(M{1})|(y{4})|(y{2})|(D{2})|(D{1})|(d{2})|(d{1})|(H{2})|(h{2})|(h{1})|(m{2})|(m{1})|(s{2})|(s{1}|(f{2})|(f{1})|(TT{1})|(tt{1})|(Z{1})|(zzz{1})|(zz{1}))`)
+	for _, match := range regex.FindAllString(resultFormat, -1) {
+		for key, val := range predefs {
 			if match == key {
-				pformat = strings.Replace(pformat, match, val, -1)
+				resultFormat = strings.Replace(resultFormat, match, val, -1)
 			}
 		}
 	}
-
-	timeriz := k.Time.Format(pformat)
-	return timeriz
-
+	if d.Year != 0 {
+		if d.Day == 0 {
+			d.Day = 1
+		}
+		if d.Month == 0 {
+			d.Month = 1
+		}
+		return d.New(d.Year, d.Month, d.Day, d.Hour, d.Min, d.Sec, d.Nsec).Time.Format(resultFormat)
+	}
+	return d.Time.Format(resultFormat)
 }
 
-//Now - Retrieve current datetime
-func Now() *SKair {
-	var k SKair
-	k.Time = time.Now()
-	return &k
+// New function returns a DateTime instance from specified date and/or time values
+//
+// Overloads:
+// New(year, month, day)
+// New(year, month, day, hours)
+// New(year, month, day, hours, minutes)
+// New(year, month, day, hours, minutes, seconds)
+// New(year, month, day, hours, minutes, seconds, nanoseconds)
+func (DateTime) New(args ...interface{}) *DateTime {
+	var d DateTime
+	switch len(args) {
+	case 3: // New(year, month, day)
+		d.Time = time.Date(args[0].(int), months[args[1].(int)], args[2].(int), 0, 0, 0, 0, time.UTC)
+	case 4: // New(year, month, day, hours)
+		d.Time = time.Date(args[0].(int), months[args[1].(int)], args[2].(int), args[3].(int), 0, 0, 0, time.UTC)
+	case 5: // New(year, month, day, hours, minutes)
+		d.Time = time.Date(args[0].(int), months[args[1].(int)], args[2].(int), args[3].(int), args[4].(int), 0, 0, time.UTC)
+	case 6: // New(year, month, day, hours, minutes, seconds)
+		d.Time = time.Date(args[0].(int), months[args[1].(int)], args[2].(int), args[3].(int), args[4].(int), args[5].(int), 0, time.UTC)
+	case 7: // New(year, month, day, hours, minutes, seconds, nanoseconds)
+		d.Time = time.Date(args[0].(int), months[args[1].(int)], args[2].(int), args[3].(int), args[4].(int), args[5].(int), args[6].(int), time.UTC)
+	default:
+		panic("invalid function arguments")
+	}
+	return &d
 }
 
-//Date - Retrieves a custom date
-func Date(day int, month int, year int) *SKair {
-	var k SKair
-	k.Time = time.Date(year, months[month], day, 0, 0, 0, 0, time.UTC)
-	return &k
+// NewDateTime function returns a DateTime instance from specified date and/or time values
+//
+// Overloads:
+// NewDateTime(year, month, day)
+// NewDateTime(year, month, day, hours)
+// NewDateTime(year, month, day, hours, minutes)
+// NewDateTime(year, month, day, hours, minutes, seconds)
+// NewDateTime(year, month, day, hours, minutes, seconds, nanoseconds)
+func NewDateTime(args ...interface{}) *DateTime {
+	return DateTime{}.New(args...)
 }
 
-// DateTime - Retrieves a custom datetime
-func DateTime(day int, month int, year int, hour int, min int, sec int) *SKair {
-	var k SKair
-	k.Time = time.Date(year, months[month], day, hour, min, sec, 0, time.UTC)
-	return &k
+// Now function returns DateTime instance from specified date and/or time values
+func (DateTime) Now() *DateTime {
+	return &DateTime{Time: time.Now()}
+}
+
+// Add Returns a new DateTime that adds the value of the specified date values to the value of this instance.
+func (d DateTime) Add(years, months, days int) *DateTime {
+	return &DateTime{Time: d.Time.AddDate(years, months, days)}
+}
+
+// AddDays Returns a new DateTime that adds the specified number of days to the value of this instance.
+func (d DateTime) AddDays(value int) *DateTime {
+	return &DateTime{Time: d.Time.AddDate(0, 0, value)}
+}
+
+// AddMonths Returns a new DateTime that adds the specified number of months to the value of this instance.
+func (d DateTime) AddMonths(value int) *DateTime {
+	return &DateTime{Time: d.Time.AddDate(0, value, 0)}
+}
+
+// AddYears Returns a new DateTime that adds the specified number of years to the value of this instance.
+func (d DateTime) AddYears(value int) *DateTime {
+	return &DateTime{Time: d.Time.AddDate(value, 0, 0)}
+}
+
+// AddTime Returns a new DateTime that adds the specified time values to the value of this instance.
+func (d DateTime) AddTime(hours, minutes, seconds int) *DateTime {
+	return &DateTime{Time: d.Time.Add(time.Hour*time.Duration(hours) + time.Minute*time.Duration(minutes) + time.Second*time.Duration(seconds))}
+}
+
+// AddHours Returns a new DateTime that adds the specified number of hours to the value of this instance.
+func (d DateTime) AddHours(value int) *DateTime {
+	return &DateTime{Time: d.Time.Add(time.Hour * time.Duration(value))}
+}
+
+// AddMinutes Returns a new DateTime that adds the specified number of minutes to the value of this instance.
+func (d DateTime) AddMinutes(value int) *DateTime {
+	return &DateTime{Time: d.Time.Add(time.Minute * time.Duration(value))}
+}
+
+// AddSeconds Returns a new DateTime that adds the specified number of seconds to the value of this instance.
+func (d DateTime) AddSeconds(value int) *DateTime {
+	return &DateTime{Time: d.Time.Add(time.Second * time.Duration(value))}
+}
+
+// AddMilliseconds Returns a new DateTime that adds the specified number of milliseconds to the value of this instance.
+func (d DateTime) AddMilliseconds(value int) *DateTime {
+	return &DateTime{Time: d.Time.Add(time.Millisecond * time.Duration(value))}
+}
+
+// AddMicroseconds Returns a new DateTime that adds the specified number of microseconds to the value of this instance.
+func (d DateTime) AddMicroseconds(value int) *DateTime {
+	return &DateTime{Time: d.Time.Add(time.Microsecond * time.Duration(value))}
+}
+
+// Subtract Returns a new DateTime that subtracts the specified date values from the value of this instance.
+func (d DateTime) Subtract(years, months, days int) *DateTime {
+	return &DateTime{Time: d.Time.AddDate(-years, -months, -days)}
+}
+
+// SubtractTime Returns a new DateTime that subtracts the specified time values from the value of this instance.
+func (d DateTime) SubtractTime(hours, minutes, seconds int) *DateTime {
+	return &DateTime{Time: d.Time.Add(-(time.Hour*time.Duration(hours) + time.Minute*time.Duration(minutes) + time.Second*time.Duration(seconds)))}
 }
--- kair_test.go
+++ kair_test.go
@@ -1,79 +1,191 @@
-package kair
+package sharpdate
 
 import (
-	"fmt"
 	"testing"
 	"time"
 )
 
-//TestPersonalDate - Check KDate func return
-func TestPersonalDate(t *testing.T) {
-	aR := Date(20, 05, 2018)
-	eR := "2018-05-20 00:00:00 +0000 UTC"
-	if aR.Time.String() != eR {
-		t.Fatalf("Expected %s but go %s", eR, aR)
-	} else {
-		fmt.Println("TestPersonalDate - OK")
+func TestDateTimeAddDays(t *testing.T) {
+	expected := "31-12-2018 23:55:59"
+	actual := NewDateTime(2018, 12, 28, 23, 55, 59).AddDays(3).ToString("dd-MM-yyyy HH:mm:ss")
+	if expected != actual {
+		t.Errorf("\nExpected '%s' but got '%s' instead.", expected, actual)
 	}
 }
 
-//TestPersonalDateTime - Check KDateTime func return
-func TestPersonalDateTime(t *testing.T) {
-	aR := DateTime(20, 05, 2018, 14, 20, 30)
-	eR := "2018-05-20 14:20:30 +0000 UTC"
-	if aR.Time.String() != eR {
-		t.Fatalf("Expected %s but go %s", eR, aR)
-	} else {
-		fmt.Println("TestPersonalDateTime - OK")
+func TestDateTimeAddMonths(t *testing.T) {
+	expected := "01-05-2018 23:55:59"
+	actual := NewDateTime(2018, 2, 1, 23, 55, 59).AddMonths(3).ToString("dd-MM-yyyy HH:mm:ss")
+	if expected != actual {
+		t.Errorf("\nExpected '%s' but got '%s' instead.", expected, actual)
 	}
 }
 
-//TestNowGetter - Check KFormat func return
-func TestNowGetter(t *testing.T) {
-	aR := Now().Format("LLL")
-	eR := time.Now().Format("January 2, 2006 3:04 PM")
-	if aR != eR {
-		t.Fatalf("Expected %s but go %s", eR, aR)
-	} else {
-		fmt.Println("TestNowGetter - OK")
+func TestDateTimeAddYears(t *testing.T) {
+	expected := "01-05-2018 23:55:59"
+	actual := NewDateTime(2015, 5, 1, 23, 55, 59).AddYears(3).ToString("dd-MM-yyyy HH:mm:ss")
+	if expected != actual {
+		t.Errorf("\nExpected '%s' but got '%s' instead.", expected, actual)
 	}
 }
 
-//TestKFormat - Check all possible default format results
-func TestKFormat(t *testing.T) {
-	date := DateTime(20, 05, 2018, 10, 20, 0)
-	testList := map[string]string{
-		"LT":   "10:20 AM",
-		"LTS":  "10:20:00 AM",
-		"L":    "20/05/2018",
-		"l":    "20/5/2018",
-		"LL":   "May 20, 2018",
-		"ll":   "May 20, 2018",
-		"LLL":  "May 20, 2018 10:20 AM",
-		"lll":  "May 20, 2018 10:20 AM",
-		"LLLL": "Sunday, May 20, 2018 10:20 AM",
-		"llll": "Sun, May 20, 2018 10:20 AM",
-		"":     "2018-05-20 10:20:00 +0000 UTC",
+func TestDateTimeAdd(t *testing.T) {
+	expected := "03-05-2018 23:55:59"
+	actual := NewDateTime(2015, 4, 1, 23, 55, 59).Add(3, 1, 2).ToString("dd-MM-yyyy HH:mm:ss")
+	if expected != actual {
+		t.Errorf("\nExpected '%s' but got '%s' instead.", expected, actual)
 	}
+}
 
-	for i, v := range testList {
-		aR := date.Format(i)
-		eR := v
-		if aR != eR {
-			t.Fatalf("Expected %s but go %s", eR, aR)
+func TestDateTimeSubtract(t *testing.T) {
+	expected := "01-04-2015 22:50:50"
+	actual := NewDateTime(2018, 5, 3, 22, 50, 50).Subtract(3, 1, 2).ToString("dd-MM-yyyy HH:mm:ss")
+	if expected != actual {
+		t.Errorf("\nExpected '%s' but got '%s' instead.", expected, actual)
+	}
+}
+
+func TestDateTimeAddTime(t *testing.T) {
+	expected := "03-05-2018 23:55:59"
+	actual := NewDateTime(2018, 5, 3, 22, 50, 50).AddTime(1, 5, 9).ToString("dd-MM-yyyy HH:mm:ss")
+	if expected != actual {
+		t.Errorf("\nExpected '%s' but got '%s' instead.", expected, actual)
+	}
+}
+
+func TestDateTimeSubtractTime(t *testing.T) {
+	expected := "03-05-2018 22:50:50"
+	actual := NewDateTime(2018, 5, 3, 23, 55, 59).SubtractTime(1, 5, 9).ToString("dd-MM-yyyy HH:mm:ss")
+	if expected != actual {
+		t.Errorf("\nExpected '%s' but got '%s' instead.", expected, actual)
+	}
+}
+
+func TestDateTimeAddMinutes(t *testing.T) {
+	expected := "03-05-2018 23:55:59"
+	actual := NewDateTime(2018, 5, 3, 23, 45, 59).AddMinutes(10).ToString("dd-MM-yyyy HH:mm:ss")
+	if expected != actual {
+		t.Errorf("\nExpected '%s' but got '%s' instead.", expected, actual)
+	}
+}
+
+func TestDateTimeAddHours(t *testing.T) {
+	expected := "03-05-2018 23:55:59"
+	actual := NewDateTime(2018, 5, 3, 22, 55, 59).AddHours(1).ToString("dd-MM-yyyy HH:mm:ss")
+	if expected != actual {
+		t.Errorf("\nExpected '%s' but got '%s' instead.", expected, actual)
+	}
+}
+
+func TestDateTimeAddSeconds(t *testing.T) {
+	expected := "03-05-2018 23:55:59"
+	actual := NewDateTime(2018, 5, 3, 23, 55, 49).AddSeconds(10).ToString("dd-MM-yyyy HH:mm:ss")
+	if expected != actual {
+		t.Errorf("\nExpected '%s' but got '%s' instead.", expected, actual)
+	}
+}
+
+func TestDateTimeAddMilliseconds(t *testing.T) {
+	expected := "03-05-2018 23:55:59.010"
+	actual := NewDateTime(2018, 5, 3, 23, 55, 59, 0).AddMilliseconds(10).ToString("dd-MM-yyyy HH:mm:ss.f")
+	if expected != actual {
+		t.Errorf("\nExpected '%s' but got '%s' instead.", expected, actual)
+	}
+}
+
+func TestDateTimeAddMicroseconds(t *testing.T) {
+	expected := "03-05-2018 23:55:59.000011"
+	actual := NewDateTime(2018, 5, 3, 23, 55, 59, 0).AddMicroseconds(11).ToString("dd-MM-yyyy HH:mm:ss.ff")
+	if expected != actual {
+		t.Errorf("\nExpected '%s' but got '%s' instead.", expected, actual)
+	}
+}
+
+func TestNewDateTimeToString(t *testing.T) {
+	expected := "31-12-2018 23:55:59"
+	actual := NewDateTime(2018, 12, 31, 23, 55, 59).ToString("dd-MM-yyyy HH:mm:ss")
+	if expected != actual {
+		t.Errorf("\nExpected '%s' but got '%s' instead.", expected, actual)
+	}
+}
+
+func TestNewDateTimeNoTimeToString(t *testing.T) {
+	expected := "31-12-2018"
+	actual := NewDateTime(2018, 12, 31).ToString("dd-MM-yyyy")
+	if expected != actual {
+		t.Errorf("\nExpected '%s' but got '%s' instead.", expected, actual)
+	}
+}
+
+func TestNewDateTimeShortTimeToString(t *testing.T) {
+	expected := "31-12-2018 23"
+	actual := NewDateTime(2018, 12, 31, 23).ToString("dd-MM-yyyy HH")
+	if expected != actual {
+		t.Errorf("\nExpected '%s' but got '%s' instead.", expected, actual)
+	}
+}
+
+func TestNewDateTimeShortTimeMinutesToString(t *testing.T) {
+	expected := "31-12-2018 23:55"
+	actual := NewDateTime(2018, 12, 31, 23, 55).ToString("dd-MM-yyyy HH:mm")
+	if expected != actual {
+		t.Errorf("\nExpected '%s' but got '%s' instead.", expected, actual)
+	}
+}
+
+func TestDateTimeStructToString(t *testing.T) {
+	expected := "31-12-2018 23:55:59"
+	actual := DateTime{Year: 2018, Month: 12, Day: 31, Hour: 23, Min: 55, Sec: 59}.ToString("dd-MM-yyyy HH:mm:ss")
+	if expected != actual {
+		t.Errorf("\nExpected '%s' but got '%s' instead.", expected, actual)
+	}
+}
+
+func TestDateTimeNewToString(t *testing.T) {
+	expected := "31-12-2018 23:55:59"
+	actual := DateTime{}.New(2018, 12, 31, 23, 55, 59).ToString("dd-MM-yyyy HH:mm:ss")
+	if expected != actual {
+		t.Errorf("\nExpected '%s' but got '%s' instead.", expected, actual)
+	}
+}
+
+func TestDateTimeNowToString(t *testing.T) {
+	expected := time.Now().Format("02-01-2006 15:04:05")
+	actual := DateTime{}.Now().ToString("dd-MM-yyyy HH:mm:ss")
+	if expected != actual {
+		t.Errorf("\nExpected '%s' but got '%s' instead.", expected, actual)
+	}
+}
+
+func TestDateTimeUnix(t *testing.T) {
+	expected := time.Now().Unix()
+	actual := DateTime{}.Now().Time.Unix()
+	if expected != actual {
+		t.Errorf("\nExpected '%v' but got '%v' instead.", expected, actual)
+	}
+}
+
+func TestDateTimeYearMonth(t *testing.T) {
+	expected := "12-2018"
+	actual := DateTime{Year: 2018, Month: 12}.ToString("MM-yyyy")
+	if expected != actual {
+		t.Errorf("\nExpected '%v' but got '%v' instead.", expected, actual)
+	}
+}
+
+func TestDateTimeYear(t *testing.T) {
+	expected := "2018"
+	actual := DateTime{Year: 2018}.ToString("yyyy")
+	if expected != actual {
+		t.Errorf("\nExpected '%v' but got '%v' instead.", expected, actual)
+	}
+}
+
+func TestNewDateTimeInvalidArguments(t *testing.T) {
+	defer func() {
+		if r := recover(); r == nil {
+			t.Errorf("\nExpected panic but got no panic instead!")
 		}
-	}
-	fmt.Println("TestKFormat - OK")
-}
-
-//CustomFormat - Checks whether custom return is correct
-func TestPersonalFormat(t *testing.T) {
-	date := DateTime(20, 05, 2018, 10, 20, 0)
-	aR := date.CustomFormat("MMM/dd/YY h:m:s")
-	eR := "May/20/18 10:20:0"
-	if aR != eR {
-		t.Fatalf("Expected %s but go %s", eR, aR)
-	} else {
-		fmt.Println("TestPersonalFormat - OK")
-	}
+	}()
+	NewDateTime("2018").ToString("yyyy")
 }
